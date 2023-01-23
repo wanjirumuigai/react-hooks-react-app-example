b679e682f89ff459ef5806252bbc1fbc21552aa8
Pauline Muigai <wanjirumuigai@gmail.com>
2023-01-23 14:18:03 +0300

    completed assignment

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -8,6 +8,7 @@ function ExampleComponent() {
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
+      
       <p>Whoa!</p>
     </div>
   );
